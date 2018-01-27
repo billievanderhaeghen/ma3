8ed89dbba3f14d752284ba8fe7a5a09065409db6
Billie Vanderhaeghen <billie.vanderhaeghen@student.howest.be>
2018-01-27 12:00:00 +0100

    issue 4: css mobile detail page

--- src/view/events/detail.php
+++ src/view/events/detail.php
@@ -12,39 +12,47 @@
 <main>
   <section class="details-section">
       <article class="details-article">
-        <div class="breadcrumbs">
-          <a href="index.php?page=agenda">Agenda</a> >
-          <a href="index.php?page=agenda&day=<?php echo $day['id'] ?>"><?php echo $day['name'] ?></a> >
-          <a href="index.php?page=detail&amp;id=<?php echo $event['id'] ?>"><?php echo $event['title'] ?></a>
+        <header class="details-header">
+          <h2 class="detail-header-title"><?php echo $event['title']; ?></h2>
+          <div class="breadcrumbs">
+            <a href="index.php?page=agenda">Agenda</a> >
+            <a href="index.php?page=agenda&day=<?php echo $day['id'] ?>"><?php echo $day['name'] ?></a> >
+            <a href="index.php?page=detail&amp;id=<?php echo $event['id'] ?>"><?php echo $event['title'] ?></a>
+          </div>
+        </header>
+        <div class="details-img-div">
+          <img class="details-img"
+              src="assets/img/events/<?php echo $event["code"];?>/1.jpg"
+              height="214" width="320"
+               alt="blog-image">
         </div>
-        <header><h2 class="detail-header-title"><?php echo $event['title']; ?></h2></header>
-        <img class="event-card-img"
-            src="assets/img/events/<?php echo $event["code"];?>/1.jpg"
-            height="214" width="320"
-             alt="blog-image">
        <div class="details-text">
          <p class="details-content"><?php echo $event['content'];?></p>
          <p class="details-practical"><?php echo $event['practical'];?></p>
        </div>
-        <div class="details">
-          <header>
-            <h3 class="detail-header-h3"><?php echo $day['name'] ?></h3>
-          </header>
-          <div class="details-div">
-            <p class="details-address"><?php echo $event['address'];?>, <?php echo $event['city'];?></p>
-            <?php $date = date('d/m', strtotime($event['start'])); $time = date('h:i', strtotime($event['start'])); ?>
-            <p class="details-datum"><?php echo $date;?></p>
-            <p class="details-uur"><?php echo $time;?></p>
-            <p class="details-link"><?php echo $event['link'];?></p>
-            <ul>
-              <?php foreach($tags as $tag): ?>
-                    <?php foreach ($tag as $t): ?>
-                      <li><?php echo $t['tag'];?></li>
-                    <?php endforeach;?>
-                  <?php endforeach;?>
-            </ul>
-          </div>
-        </div>
+       <section class="details-section">
+         <div class="details">
+           <header>
+             <h3 class="detail-header-h3"><?php echo $day['name'] ?></h3>
+           </header>
+           <div class="details-div">
+             <p class="details-address"><?php echo $event['address'];?>, <?php echo $event['city'];?></p>
+             <?php $date = date('d/m', strtotime($event['start'])); $time = date('h:i', strtotime($event['start'])); ?>
+             <div class="details-date">
+               <p class="details-datum"><?php echo $date;?></p>
+               <p class="details-uur"><?php echo $time;?></p>
+             </div>
+             <a class="details-link" href="<?php echo $event['link'];?>">over organisatie</a>
+             <ul class="details-tags">
+               <?php foreach($tags as $tag): ?>
+                     <?php foreach ($tag as $t): ?>
+                       <li class="details-tag"><?php echo $t['tag'];?></li>
+                     <?php endforeach;?>
+                   <?php endforeach;?>
+             </ul>
+           </div>
+         </div>
+       </section>
       </article>
       <section class="related-events">
 
